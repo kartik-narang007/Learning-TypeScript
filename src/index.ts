da3a3adf4f4009f0a24b527a3eec91cb98741f65
Kartik Narang <narangk007@gmail.com>
2023-10-13 17:28:07 +0530

    enums in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-// tuples in typeScript
+// enums in typescript
 
-let user: [number,string] = [23, 'kartik'];
+//enums are a list of related constants
+
+//let's say we want to represent size of tshirts one way to represent is 
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+//another way to group these constants in enums
+
+const enum Size {Small = 1, Medium = 2, Large = 3}; //we can also use string value here
+let mySize: Size= Size.Medium;
+
+console.log(mySize);
+
+//if we define this enum as constant the compiler will generate a more optimized code
+
+//variable name: type = value(Size.medium)
