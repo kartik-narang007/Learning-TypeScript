db7fd40406416cffc032203117d523cbadac1ed6
Kartik Narang <narangk007@gmail.com>
2023-10-13 17:12:30 +0530

    Tuples in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-//arrays in typeScript
+// tuples in typeScript
 
-// in javaScript an array can contains any type of value in a single array but in TS we have to specify the type of array elements
-
-let numbers: number[] = [1,2,3];
+let user: [number,string] = [23, 'kartik'];
