d7627f72f73a34fa3c25d492466a227b2d6eb3e5
Kartik Narang <narangk007@gmail.com>
2023-10-15 13:00:10 +0530

    The Never Type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-// The Unknown Type
+// The Never Type
 
-function render(document: unknown) {
-     // Narrowing
-
-    if(/*typeof document === 'string'*/ document instanceof wordDocument){
-        document.toUpperCase();
-     }
+function processEvents(): never {
+    while(true) {
+        //Read a message from the queue
+    }
 }
+
+processEvents();
+console.log('Hello World'); //this line will never get executed because upper function never returns
+
+//agar mai yaha se never hata du to hame kabhi warning nahi aayegi ki last line execute nahi hogi yaha never keyword help krta hai hamein warning dene me
+
+// is warning ko on krne ke liye allowunreachablecode ko configuration me false krna pdega
