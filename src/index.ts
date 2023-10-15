45b190d8cafae2269651da25562c550e829804e0
Kartik Narang <narangk007@gmail.com>
2023-10-15 12:12:52 +0530

    Types union in TS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-//Type Aliases in TS
+//union types in TS
 
-type Employee = {
-    readonly id: number,
-    name: string,
-    retire: (date:Date) => void
-}
-
-let employee: Employee = {
-    id: 1,
-    name: 'Mosh',
-    retire:(date: Date)=>{
-        console.log(date);
+function kgToLbs(weight: number | string): number {
+    //Narrowing
+    if(typeof weight === 'number'){
+        return weight * 2.2;
+    }
+    else{
+        return parseInt(weight) * 2.2;
     }
 }
+
+kgToLbs(10);
+kgToLbs('10');
