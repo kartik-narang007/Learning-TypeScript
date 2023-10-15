6e7f9dc6d23091b0c02c341e45159c197d590116
Kartik Narang <narangk007@gmail.com>
2023-10-15 17:57:11 +0530

    Index Signatures

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,10 @@
-//Classes in TS
+// Index Signatures in TS
 
-class Account {
-    // readonly id: number;
-    // owner: string;
-    // private balance: number;
-    nickname?: string;
+class SeatAssignment {
+    [seatNumber: string]: string;
+}
 
-    constructor(
-        public readonly id: number,
-        public owner: string,
-        private _balance: number
-    )
-    {
-        // this.id = id;
-        // this.owner = owner;
-        // this.balance = balance;
-    };
+let seats = new SeatAssignment();
 
-    deposit ( amount : number ) : void{
-        if(amount <= 0){
-            throw new Error('invalid amount')
-        }
-        else{
-            this._balance += amount;
-        }
-    }
-    
-    get balance(): number{
-        return this.balance;
-    }
-    set balance(value: number) {
-        if(value<0)
-            throw new Error('Invalid Value');
-        this._balance = value;
-    }
-};
-
-
-
-
-let account = new Account(1,'kartik',0);
-account.deposit(100);
-console.log(account instanceof Account);
-// console.log(account.balance) won't work since balance is a private variable
-console.log(account.balance);
+seats.A1 = 'kartik';
+seats.A2 = 'premdeep';
