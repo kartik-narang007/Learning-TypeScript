1d2b5ebd4252b732899fbb43d10cae2bef2020f1
Kartik Narang <narangk007@gmail.com>
2023-10-15 12:40:13 +0530

    Optional Chaining in TS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-//Nullable Types in TS
+//Optional Chaining Types in TS
 
-function greet(name: string | null | undefined) {
-    if(name){
-    console.log(name.toUpperCase());
-    }
-    else{
-        console.log('Hola!');
-    }
+type Customer = {
+    birthday?: Date
+};
+function getCustomer(id: number) : Customer | null | undefined {
+
+    return id === 0 ? null : {birthday: new Date()};
 }
 
-greet(null);
+let customer =  getCustomer(0);
+// if(customer !== null && customer !== undefined)
+//Optional property access operator
+
+    console.log(customer?.birthday?.getFullYear());
+
+
+//Optional element access operator
+// if(customer !== null && customer !== undefined)
+// short form     customers?.[0]
+
+
+//optional call operator
+
+// let log: any = (message: string) => console.log(message); 
+
+let log: any = null;
+
+log?.('a');
