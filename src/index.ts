cf0b42f78be369276b15707fdce61462a47591d4
Kartik Narang <narangk007@gmail.com>
2023-10-16 14:55:01 +0530

    Abstract Classes and Methods

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,13 @@
-class Person {
-    constructor(public firstName: string, public lastName: string) {}
-
-    get fullName() {
-        return this.firstName + ' ' + this.lastName;
-    }
-
-    walk() {
-            console.log('Walking');
-    }
+abstract class Shape{
+    constructor(public color: string){}
+    abstract render() : void;
 }
 
-class Student extends Person {
-    constructor(public studentId: number, firstName: string, lastName: string) {
-        super(firstName, lastName);
+class Circle extends Shape{
+    constructor(public radius: number, color: string){
+        super(color);
     }
-
-    takeTest() {
-        console.log('Taking a test');
+    override render() : void{
+        console.log('rendering a circle')
     }
-}
-
-class Teacher extends Person {
-    override get fullName(): string {
-        return 'Professor ' + super.fullName;
-    }
-}
-
-class Principal extends Person{
-    override get fullName(): string{
-        return 'Principal ' + super.fullName
-    }
-}
-
-printNames([
-    new Student(1, 'Kartik', 'Narang'),
-    new Teacher('Kartik', 'Narang'),
-    new Principal('Kartik', 'Narang')
-
-])
-
-function printNames(people: Person[]){
-    for(let person of people)
-        console.log(person.fullName);
 }
